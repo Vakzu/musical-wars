292b81c5a6c3aec3b13572d21ae3ebe8ddbc6159
dmittrey <dmitryzubakhin@mail.ru>
2022-12-06 02:44:10 +0300

    Add delete tables script

---
+++ src/main/resources/sql_scripts/tables_ops/cleanup.sql
@@ -0,0 +1,15 @@
+DROP TABLE "Location" IF EXIST;
+
+DROP TABLE "Fight" IF EXIST;
+
+DROP TABLE "Song" IF EXIST;
+
+DROP TABLE "Effect" IF EXIST;
+
+DROP TABLE "Hero" IF EXIST;
+
+DROP TABLE "Character" IF EXIST;
+
+DROP TABLE "User" IF EXIST;
+
+DROP TABLE "Fight_Participant" IF EXIST;
