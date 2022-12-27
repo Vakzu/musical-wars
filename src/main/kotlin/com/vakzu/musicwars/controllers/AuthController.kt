eb628848f06fad1fe466919cef991815949408b7
Danil Vakhrushev <danil1209@yandex.ry>
2022-12-27 11:07:15 +0300

    Added new songs and heroes, Removed unnecessary test data insertions Removed unnecessary classes

--- src/main/kotlin/com/vakzu/musicwars/controllers/AuthController.kt
+++ src/main/kotlin/com/vakzu/musicwars/controllers/AuthController.kt
@@ -3,12 +3,16 @@ package com.vakzu.musicwars.controllers
 import com.vakzu.musicwars.dto.LoginDto
 import com.vakzu.musicwars.dto.OnlineUserDto
 import com.vakzu.musicwars.dto.RegisterRequest
+import com.vakzu.musicwars.exceptions.UserAlreadyExistsException
 import com.vakzu.musicwars.security.MyUserPrincipal
 import com.vakzu.musicwars.security.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
 
@@ -30,6 +34,12 @@ class AuthController(val userService: UserService) {
     @GetMapping("/user/online")
     fun getOnlineUsers(principal: Principal): List<OnlineUserDto> {
         return userService.getOnlineUsers().map { OnlineUserDto(it.id, it.name) }
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException::class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    fun userAlreadyExists(e: UserAlreadyExistsException): String? {
+        return e.message
     }
 
 }
--- src/main/resources/sql_scripts/insertion.sql
+++ src/main/resources/sql_scripts/insertion.sql
@@ -5,7 +5,8 @@ VALUES ('Vienna'),
        ('Tokyo'),
        ('Paris'),
        ('Salzburg'),
-       ('Moscow');
+       ('Moscow'),
+       ('Uryupinsk');
 
 INSERT INTO "hero" ("name", "price", "health", "img_path")
 VALUES ('Wolfgang Amadeus Mozart', 300, 100, 'mozart.jpg'),
@@ -14,7 +15,9 @@ VALUES ('Wolfgang Amadeus Mozart', 300, 100, 'mozart.jpg'),
        ('Pyotr Ilyich Tchaikovsky', 600, 170, 'tchaikovsky.jpg'),
        ('Frederic Chopin', 700, 200, 'chopin.jpg'),
        ('Dora', 10000, 999999, 'dora.jpg'),
-       ('Samuel Kim', 10, 200, 'samuel.jpg'); -- jojo no music author
+       ('Neuro girl', 500, 148, 'generation.png'),
+       ('Ultra cow', 1000, 202, 'cow.jpg');
+
 
 INSERT INTO "song" ("name", "experience_level", "hero_id", "damage")
 VALUES ('Ballade No. 1 in G minor Op. 23', 100, 5, 45),
@@ -25,60 +28,14 @@ VALUES ('Ballade No. 1 in G minor Op. 23', 100, 5, 45),
        ('Dorafool', 0, 6, 41),
        ('Fell In Love', 0, 6, 43),
        ('Pink hair', 10, 6, 43),
-       ('Pillar Men Theme', 0, 7, 43);
+       ('Different', 0, 7, 38),
+       ('Dead man"ns bones', 0, 8, 35),
+       ('Say what you like', 0, 7, 26),
+       ('Never say goodbye', 0, 8, 42);
 
 INSERT INTO "effect" ("name", "price", "stamina", "strength", "luck", "constitution")
 VALUES ('Death 13', 50, 0, 4, 0, 1),
        ('Sticky fingers', 70, 4, 0, 1, 1),
        ('Lucky bastard', 65, 1, 0, 5, 0),
-       ('Stop me pls', 70, 1, 5, 5, 0),
-       ('Stop him ahaha', 5, 5, 0, 5, 0);
-
-INSERT INTO "user" ("name", "is_online", "password_hash")
-VALUES ('Petya', true, '213192873123978'),
-       ('Nastya', true, '1927398127391273'),
-       ('Katya', true, '1927398172937123'),
-       ('Lesya', true, '1827391723791273'),
-       ('Grenka', true, '9871203791273'),
-       ('Ohmy', true, '1231928030128398213'),
-       ('Dmittrey', false, '012983091823081023'),
-       ('ohohoh', false, '09182309120938091283');
-
-INSERT INTO "character" ("experience", "hero_id", "user_id")
-VALUES (0, 6, 1),
-       (5, 3, 1),
-       (60, 7, 2),
-       (12, 3, 3),
-       (45, 4, 4),
-       (67, 5, 5),
-       (3, 6, 1),
-       (2, 2, 2),
-       (56, 1, 2),
-       (23, 6, 2),
-       (11, 4, 3),
-       (12, 5, 4),
-       (67, 3, 5),
-       (90, 4, 6),
-       (11, 4, 3),
-       (11, 5, 3),
-       (11, 6, 3),
-       (11, 7, 3),
-       (11, 4, 4),
-       (11, 4, 5),
-       (11, 4, 6),
-       (11, 4, 7),
-       (11, 4, 7);
-
-INSERT INTO "inventory" ("user_id", "effect_id", "amount")
-VALUES (1, 2, 1),
-       (2, 3, 5),
-       (3, 1, 4),
-       (4, 2, 3),
-       (5, 3, 6),
-       (1, 2, 6),
-       (2, 2, 6),
-       (3, 1, 2),
-       (4, 1, 1),
-       (5, 3, 3),
-       (6, 3, 4),
-       (7, 3, 5);
+       ('Walking corpse', 60, 0, 0, 0, 5),
+       ('Can you kill me?', 55, 0, 0, 3, 3);
